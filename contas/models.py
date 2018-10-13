f44a7ce0c76c7156a670c3ae89fd18e18ed3948c
Dimsol <claudiosakaeshigemi@gmail.com>
2018-10-13 16:22:08 -0300

    seção 3 aula 16 - update
    seção 3 aula 16 - update

--- contas/models.py
+++ contas/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Categoria(models.Model):
     nome = models.CharField(max_length = 100)
--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
+from .form import TransacaoForm
 import datetime
 
 
@@ -17,3 +18,28 @@ def listagem(request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render(request,'contas/listagem.html',data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
+
+def update(request, pk ):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
